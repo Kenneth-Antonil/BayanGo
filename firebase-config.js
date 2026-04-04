143d85faa1d05fd95e54f625002ad8f1fb3a60da
Kenneth-Antonil <payment.kraph@gmail.com>
2026-04-04 11:10:24 +0800

    Set Firebase authDomain to auth.bayango.store

--- firebase-config.js
+++ firebase-config.js
@@ -4,7 +4,7 @@ import { getMessaging, isSupported } from 'firebase/messaging';
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyC0AQ3rkH-fhxlThobjDDtrvxttAfFLXTE',
-  authDomain: 'bayango-315c6.firebaseapp.com',
+  authDomain: 'auth.bayango.store',
   databaseURL: 'https://bayango-315c6-default-rtdb.asia-southeast1.firebasedatabase.app',
   projectId: 'bayango-315c6',
   storageBucket: 'bayango-315c6.firebasestorage.app',
